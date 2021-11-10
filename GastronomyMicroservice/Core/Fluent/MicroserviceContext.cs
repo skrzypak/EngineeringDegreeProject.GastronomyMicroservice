a6ad443fbb613e13e3543416192c893f1c8db772
Konrad Skrzypczyk <skrzypczykmail@gmail.com>
2021-11-10 18:27:12 +0100

    Authenticaiton package sync

--- GastronomyMicroservice/Core/Fluent/MicroserviceContext.cs
+++ GastronomyMicroservice/Core/Fluent/MicroserviceContext.cs
@@ -34,8 +34,8 @@ namespace GastronomyMicroservice.Core.Fluent
             //foreach (var entity in modelBuilder.Model.GetEntityTypes())
             //{
             //    entity.AddProperty("CreatedDate", typeof(DateTime));
-            //    entity.AddProperty("EnterpriseId", typeof(ulong));
-            //    entity.AddProperty("DomainUserId", typeof(ulong));
+            //    entity.AddProperty("EspId", typeof(int));
+            //    entity.AddProperty("EduId", typeof(int));
             //}
 
             modelBuilder.ApplyConfiguration(new AllergenConfiguration());
